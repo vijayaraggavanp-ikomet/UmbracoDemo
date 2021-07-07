d6963bf81a05c9d13b926cf921f780d1b9c989df
vijayaraggavanp-ikomet <79459467+vijayaraggavanp-ikomet@users.noreply.github.com>
2021-07-08 01:44:35 +0530

    New DocType,Template(View) added

---
+++ Models/Practices.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UmbracoInternal.Models
+{
+    public class Practices
+    {
+        public string _PracticeName { get; set; }
+    }
+}
